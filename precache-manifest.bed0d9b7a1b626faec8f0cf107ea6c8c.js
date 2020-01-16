275cdd67ff333ee010771d242eab6fc68d52fd89
Harry <harrybui22@gmail.com>
2020-01-15 23:39:18 -0800

    Updates

---
+++ precache-manifest.bed0d9b7a1b626faec8f0cf107ea6c8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758c84f6b1e5f0ce4f569bfe09d8dd3d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "90dd672e344e1c739e5c",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90dd672e344e1c739e5c",
+    "url": "/my-app/static/js/main.114669a2.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
